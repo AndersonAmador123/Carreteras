cd4c40cc8b8f29a8acc77d54071e221e67cdd020
dianamurillo29 <dianamurillo2911@gmail.com>
2020-01-30 15:57:30 -0600

    Pantalla Empleados
    Revision

---
+++ Carreteras/Controllers/EmpleadosController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Carreteras.Models;
+
+namespace Carreteras.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private BD_CARRETERASEntities db = new BD_CARRETERASEntities();
+
+        // GET: Empleados
+        public ActionResult Index()
+        {
+            var tb_empleados = db.tb_empleados.Include(t => t.tb_ciudades).Include(t => t.tb_usuarios).Include(t => t.tb_usuarios1);
+            return View(tb_empleados.ToList());
+        }
+
+        // GET: Empleados/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_empleados tb_empleados = db.tb_empleados.Find(id);
+            if (tb_empleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tb_empleados);
+        }
+
+        // GET: Empleados/Create
+        public ActionResult Create()
+        {
+            ViewBag.ciu_id = new SelectList(db.tb_ciudades, "ciu_id", "ciu_descripcion");
+            ViewBag.emp_usuario_crea = new SelectList(db.tb_usuarios, "usu_id", "usu_descripcion");
+            ViewBag.emp_usuario_modifica = new SelectList(db.tb_usuarios, "usu_id", "usu_descripcion");
+            return View();
+        }
+
+        // POST: Empleados/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "emp_nombre,emp_edad,emp_telefono,ciu_id")] tb_empleados tb_empleados)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tb_empleados.Add(tb_empleados);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ciu_id = new SelectList(db.tb_ciudades, "ciu_id", "ciu_descripcion", tb_empleados.ciu_id);
+            ViewBag.emp_usuario_crea = new SelectList(db.tb_usuarios, "usu_id", "usu_descripcion", tb_empleados.emp_usuario_crea);
+            ViewBag.emp_usuario_modifica = new SelectList(db.tb_usuarios, "usu_id", "usu_descripcion", tb_empleados.emp_usuario_modifica);
+            return View(tb_empleados);
+        }
+
+        // GET: Empleados/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_empleados tb_empleados = db.tb_empleados.Find(id);
+            if (tb_empleados == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ciu_id = new SelectList(db.tb_ciudades, "ciu_id", "ciu_descripcion", tb_empleados.ciu_id);
+            ViewBag.emp_usuario_crea = new SelectList(db.tb_usuarios, "usu_id", "usu_descripcion", tb_empleados.emp_usuario_crea);
+            ViewBag.emp_usuario_modifica = new SelectList(db.tb_usuarios, "usu_id", "usu_descripcion", tb_empleados.emp_usuario_modifica);
+            return View(tb_empleados);
+        }
+
+        // POST: Empleados/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "emp_id,emp_nombre,emp_edad,emp_telefono,ciu_id,emp_usuario_crea,emp_fecha_crea,emp_usuario_modifica,emp_fecha_modifica,emp_estado")] tb_empleados tb_empleados)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tb_empleados).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ciu_id = new SelectList(db.tb_ciudades, "ciu_id", "ciu_descripcion", tb_empleados.ciu_id);
+            ViewBag.emp_usuario_crea = new SelectList(db.tb_usuarios, "usu_id", "usu_descripcion", tb_empleados.emp_usuario_crea);
+            ViewBag.emp_usuario_modifica = new SelectList(db.tb_usuarios, "usu_id", "usu_descripcion", tb_empleados.emp_usuario_modifica);
+            return View(tb_empleados);
+        }
+
+        // GET: Empleados/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_empleados tb_empleados = db.tb_empleados.Find(id);
+            if (tb_empleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tb_empleados);
+        }
+
+        // POST: Empleados/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            tb_empleados tb_empleados = db.tb_empleados.Find(id);
+            db.tb_empleados.Remove(tb_empleados);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Carreteras/Models/cEmpleados.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Carreteras.Models
+{
+    [MetadataType(typeof(cEmpleadosMetadata))]
+    public partial  class tb_empleados
+    {
+    }
+    public class cEmpleadosMetadata
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido.")]
+        [Display(Name = "Codigo")]
+        public string emp_id { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido.")]
+        [Display(Name = "Nombre")]
+        public string emp_nombre { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido.")]
+        [Display(Name = "Edad")]
+        public Nullable<int> emp_edad { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido.")]
+        [Display(Name = "Telefono")]
+        public string emp_telefono { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido.")]
+        [Display(Name = "Codigo Ciudad")]
+        public string ciu_id { get; set; } 
+        [Display(Name = "Usuario Crea")]
+        public string emp_usuario_crea { get; set; }
+        [Display(Name = "Fecha Crea")]
+        public Nullable<System.DateTime> emp_fecha_crea { get; set; }
+        [Display(Name = "Usuario Modifica")]
+        public string emp_usuario_modifica { get; set; }
+        [Display(Name = "Fecha Modifica")]
+        public Nullable<System.DateTime> emp_fecha_modifica { get; set; }
+        [Display(Name = "Estado")]
+        public Nullable<bool> emp_estado { get; set; }
+
+
+    }
+}
